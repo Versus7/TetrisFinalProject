75cf5c7a90e03ee0ffa042180c3f6a883e2e68b0
Vir <virn.shah@gmail.com>
2022-05-15 18:37:08 -0700

    Only rotates block if within bounds

--- src/IBlock.java
+++ src/IBlock.java
@@ -7,28 +7,17 @@ public class IBlock extends Piece {
 
     @Override
     public void rotateRight() {
-        Coordinate originalLowest = getCenterPoint();
-        System.out.println("Original center: " + originalLowest);
-        double degrees = Math.toRadians(-90);
-        for (int i = 0; i < 4; i++) {
-            int originalX = getShape().get(i).getCoords().getX();
-            int originalY = getShape().get(i).getCoords().getY();
-
-            getShape().get(i).getCoords().setX(Math.abs((int)(originalX*Math.cos(degrees)-originalY*Math.sin(degrees))));
-            getShape().get(i).getCoords().setY(Math.abs((int)(originalX*Math.sin(degrees)+originalY*Math.cos(degrees))));
-
+        int centerX = getCenterPoint().getX();
+        if (centerX < 8 && centerX > 1) {
+            super.rotateRight();
         }
-
-        // fix values
-        System.out.println("Rotated center: " + getCenterPoint());
-        int offsetX = originalLowest.getX() - getCenterPoint().getX();
-        int offsetY = originalLowest.getY() - getCenterPoint().getY();
-
-        for (int i = 0; i < 4; i++) {
-            getShape().get(i).changeX(offsetX);
-            getShape().get(i).changeY(offsetY);
-        }
-        System.out.println("Rotated: " + getShape().toString());
     }
 
+    @Override
+    public void rotateLeft() {
+        int centerY = getCenterPoint().getY();
+        if (centerY < 8 && centerY > 1) {
+            super.rotateLeft();
+        }
+    }
 }
