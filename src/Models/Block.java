f5d0600e4e77bd6dd62f73091d311e34871a2b45
Vir <virn.shah@gmail.com>
2022-06-06 20:58:10 -0700

    Blocks look a lot better now

--- src/Models/Block.java
+++ src/Models/Block.java
@@ -1,12 +1,12 @@
 package Models;
-import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.*;
 
 import Views.TetrisWindowPanel;
 
 public class Block {
     private Coordinate coordinate;
     private Color c;
+    private double customSize = 0;
 
     public Block(int x, int y) {
         coordinate = new Coordinate(x, y);
@@ -28,16 +28,33 @@ public class Block {
         this.c = c;
     }
 
+    public void setCustomSize(double d) {
+        customSize = d;
+    }
+
     public void draw(Graphics g) {
-        double squareSize = TetrisWindowPanel.SQUAREWIDTH;
+        Graphics2D g2 = (Graphics2D)g;
+        double squareSize;
+        if (customSize != 0) {
+            squareSize = customSize;
+        } else {
+            squareSize = TetrisWindowPanel.SQUAREWIDTH;
+        }
         double xCoord = (double)(coordinate.getX() * squareSize);
         double yCoord = (double)(coordinate.getY() * squareSize);
 
-        g.setColor(Color.black);
-        g.fillRect((int)(xCoord), (int)(yCoord), (int)(squareSize), (int)(squareSize));
+        // g.setColor(Color.black);
+        // g.fillRect((int)(xCoord), (int)(yCoord), (int)(squareSize), (int)(squareSize));
 
-        g.setColor(c);
-        g.fillRect((int)(xCoord), (int)(yCoord), (int)(TetrisWindowPanel.SQUAREWIDTH*0.95), (int)(TetrisWindowPanel.SQUAREWIDTH*0.95));
+        // g.setColor(c);
+        // g.fillRect((int)(xCoord), (int)(yCoord), (int)(squareSize*0.95), (int)(squareSize*0.95));
+        g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f));
+        g2.setColor(c);
+        g2.fillRect((int)(xCoord), (int)(yCoord), (int)squareSize, (int)squareSize);
+
+        g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
+        g.fillRect((int)(xCoord), (int)(yCoord), (int)(squareSize*0.9), (int)(squareSize*0.9));
+
     }
 
     public String toString() {
